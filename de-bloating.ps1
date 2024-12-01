6543786c1db09e9d37aa494ffa093db0a73dc351
Paulus88 <paulhemmens@live.nl>
2024-12-01 23:17:58 +0100

    Update de-bloating.ps1
    Remove Snipping Tool

--- de-bloating.ps1
+++ de-bloating.ps1
@@ -2,3 +2,5 @@ Remove-WindowsCapability -Online -Name AzureArcSetup~~~~
 Remove-WindowsFeature -Name WindowsAdminCenterSetup
 Get-AppxPackage | ?{$_.Name -like 'Microsoft.WindowsFeedbackHub*'} | Remove-AppxPackage -AllUsers
 Get-AppxProvisionedPackage -Online | ?{$_.DisplayName -Like 'Microsoft.WindowsFeedbackHub'} | Remove-AppxProvisionedPackage -Online
+Set-Location C:\Windows\System32
+SnippingTool /uninstall
