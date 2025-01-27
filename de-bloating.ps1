dfa8021799750a411e6674ccdfa1ae74e59d6e77
Paulus88 <paulhemmens@live.nl>
2025-01-27 12:25:16 +0100

    Update de-bloating.ps1

--- de-bloating.ps1
+++ de-bloating.ps1
@@ -4,3 +4,9 @@ Get-AppxPackage | ?{$_.Name -like 'Microsoft.WindowsFeedbackHub*'} | Remove-Appx
 Get-AppxProvisionedPackage -Online | ?{$_.DisplayName -Like 'Microsoft.WindowsFeedbackHub'} | Remove-AppxProvisionedPackage -Online
 Set-Location C:\Windows\System32
 SnippingTool /uninstall
+mspaint /uninstall
+mstsc /uninstall
+cd C:\Program Files (x86)\Microsoft\EdgeWebView\Application\132.0.2957.127\Installer
+setup --uninstall --msedgewebview --system-level
+cd C:\Program Files (x86)\Microsoft\Edge\Application\132.0.2957.127\Installer
+setup --uninstall --msedge --channel=stable --system-level
