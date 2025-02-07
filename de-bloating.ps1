ad161224f320aa35f5ce5e1ee988df87f6a27fd1
Paulus88 <paulhemmens@live.nl>
2025-02-07 15:47:02 +0100

    Update de-bloating.ps1

--- de-bloating.ps1
+++ de-bloating.ps1
@@ -6,5 +6,5 @@ Set-Location C:\Windows\System32
 SnippingTool /uninstall
 mspaint /uninstall
 mstsc /uninstall
-cmd /c "C:\Program Files (x86)\Microsoft\EdgeWebView\Application\132.0.2957.127\Installer\setup.exe" --uninstall --msedgewebview --system-level
-cmd /c "C:\Program Files (x86)\Microsoft\Edge\Application\132.0.2957.127\Installer\setup.exe" --uninstall --msedge --channel=stable --system-level
+cmd /c "cd C:\Program Files (x86)\Microsoft\EdgeWebView\Application\1*\Installer && setup --uninstall --msedgewebview --system-level"
+cmd /c "cd C:\Program Files (x86)\Microsoft\Edge\Application\1*\Installer && setup --uninstall --msedge --channel=stable --system-level"
